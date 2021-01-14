3a7c92b53d9e1494847fd2d56e583701be1e273b
eps1lon <silbermann.sebastian@gmail.com>
2021-01-14 23:19:29 +0100

    Reduce example

--- packages/styles/src/index.d.ts
+++ packages/styles/src/index.d.ts
@@ -1,15 +1,3 @@
-export interface CSSProperties {
-  [k: string]: unknown | CSSProperties;
-}
-
-export type StyleRules<ClassKey extends string = string> = Record<
-  ClassKey,
-  CSSProperties
->;
-export function createStyles<ClassKey extends string = string>(
-  styles: StyleRules<ClassKey>
-): StyleRules<ClassKey>;
-
 export interface StyledComponentProps<ClassKey extends string> {
   classes?: Record<ClassKey, string>;
 }
